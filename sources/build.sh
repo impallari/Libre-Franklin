dc3134b1d43be499b3bf817ff05db22864f46998
David Ross <david@djr.com>
2020-08-15 09:54:15 -0400

    build with fontbakery fixes

---
+++ sources/build.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+set -e
+
+
+echo "Generating Static fonts"
+mkdir -p ../fonts
+fontmake -m LibreFranklinRomans.designspace -i -o ttf --output-dir ../fonts/ttf/
+#fontmake -m LibreFranklinRomans.designspace -i -o otf --output-dir ../fonts/otf/
+
+fontmake -m LibreFranklinItalics.designspace -i -o ttf --output-dir ../fonts/ttf/
+#fontmake -m LibreFranklinItalics.designspace -i -o otf --output-dir ../fonts/otf/
+
+echo "Generating VFs"
+mkdir -p ../fonts/vf
+
+fontmake -m LibreFranklinRomans.designspace -o variable --output-path ../fonts/vf/LibreFranklin[wght].ttf
+
+fontmake -m LibreFranklinItalics.designspace -o variable --output-path ../fonts/vf/LibreFranklin-Italic[wght].ttf
+
+
+rm -rf master_ufo/ instance_ufo/ instance_ufos/
+
+
+echo "Post processing"
+ttfs=$(ls ../fonts/ttf/*.ttf)
+for ttf in $ttfs
+do
+	gftools fix-dsig -f $ttf;
+	#ttfautohint $ttf "$ttf.fix";
+	#mv "$ttf.fix" $ttf;
+done
+
+vfs=$(ls ../fonts/vf/*\[wght\].ttf)
+
+echo "Post processing VFs"
+for vf in $vfs
+do
+	gftools fix-dsig -f $vf;
+	#ttfautohint-vf --stem-width-mode nnn $vf "$vf.fix";
+	#mv "$vf.fix" $vf;
+done
+
+
+
+echo "Fix VF Meta"
+gftools fix-vf-meta $vfs;
+
+echo "Dropping MVAR"
+for vf in $vfs
+do
+	mv "$vf.fix" $vf;
+	ttx -f -x "MVAR" $vf; # Drop MVAR. Table has issue in DW
+	rtrip=$(basename -s .ttf $vf)
+	new_file=../fonts/vf/$rtrip.ttx;
+	rm $vf;
+	ttx $new_file
+	rm $new_file
+
+done
+
+echo "Fixing Hinting"
+for vf in $vfs
+do
+	gftools fix-nonhinting $vf "$vf.fix";
+	mv "$vf.fix" $vf;
+	#gftools fix-fsselection $vf --usetypometrics;
+done
+for ttf in $ttfs
+do
+	gftools fix-nonhinting $ttf "$ttf.fix";
+	mv "$ttf.fix" $ttf;
+	#gftools fix-fsselection $ttf --usetypometrics;
+done
+
+rm -f ../fonts/vf/*.ttx
+rm -f ../fonts/ttf/*.ttx
+rm -f ../fonts/vf/*gasp.ttf
+rm -f ../fonts/ttf/*gasp.ttf
+
+echo "Done"
