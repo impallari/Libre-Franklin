353be6f6dae457a4d65172a391124b0ef0967b9e
Denis Moyogo Jacquerye <moyogo@gmail.com>
2020-08-26 10:18:18 +0100

    build.sh: run from anywhere

--- sources/build.sh
+++ sources/build.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
 
+# Go the sources directory to run commands
+SOURCE="${BASH_SOURCE[0]}"
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+cd $DIR
+echo $(pwd)
+
+rm -rf ../fonts
+
 
 echo "Generating Static fonts"
 mkdir -p ../fonts
